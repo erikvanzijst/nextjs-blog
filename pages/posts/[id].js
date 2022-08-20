d0315574222784f8ef9f7133c19ed91ce30845c9
Erik van Zijst <erik.van.zijst@gmail.com>
2022-08-20 16:09:26 +0200

    Initial commit with NextJS demo app

---
+++ pages/posts/[id].js
@@ -0,0 +1,10 @@
+import Layout from "../../components/layout";
+
+export default function Posts() {
+    return (
+        <Layout></Layout>
+    );
+}
+
+export async function getStaticPaths() {
+}
